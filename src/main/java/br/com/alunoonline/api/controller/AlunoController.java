176adc5b5ca17b7df1a5cebd5d3e900ae76af8a0
Kelson Ameida <kelson.victr@gmail.com>
2024-09-03 21:21:08 -0300

    criarAluno funcionando

---
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,22 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAluno(@RequestBody Aluno aluno) {
+        alunoService.criarAluno(aluno);
+    }
+
+}
--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -1,5 +1,6 @@
 package br.com.alunoonline.api.service;
 
+import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class AlunoService {
 
     @Autowired
     AlunoRepository alunoRepository;
+
+    public void criarAluno(Aluno aluno) {
+        alunoRepository.save(aluno);
+    }
 }
